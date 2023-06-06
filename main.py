8feff17f7841732a6ef1d93053b205d2d75af24f
njiti <kamaukan77@gmail.com>
2023-06-06 10:39:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from fastapi.params import Body
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to my API!!!"}
+
+
+@app.get("/posts")
+def get_post():
+    return {"data": "This is your post"}
+
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post)
+    return {"data": "new post"}
+# title str, content str, category, Boolean published/draft
